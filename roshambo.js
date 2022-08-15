cf598c57ed285a0cbc5c6e2d95da8e7b47e33961
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-08-15 15:55:08 +0200

    Pseudocode for Javascript logic added

---
+++ roshambo.js
@@ -0,0 +1,19 @@
+// Ask if user wants to play, if yes, set a boolean runGame to true.
+// Initialize an array of three options (rock, paper or scissors).
+// Prompt the user for some input through an html form, a request to choose between rock, paper or scissors.
+// Store that input into a local variable.
+// Get a random number between 0-2 for the computer, and associate that number to the index of the array.
+// Store the computers chosen index option to a local variable.
+// While there is no game winner (5 wins per match) continue asking user for rock, paper or scissors.
+// Add roshambo logic through a nested if statement in the while loop.
+// If user selection beats computer selection, user wins, else computer wins.
+// Initialize two variables to store amount of wins for player and computer.
+// Send user a message to let them know if their chosen tool beats the computers or not.
+// Display amount of wins for player and amount of wins for computer.
+// Continue to run through the while loop until either the user or the computer has 5 wins.
+// If the user has 5 wins, declare that 'they won!'
+// Set runGame to false.
+// If the computer has 5 wins, declare that 'they lost :(' and add a 'new game?' option.
+// Set runGame to false
+// Ask if user wants to play another game.
+// If user selects new game set runGame to true.
