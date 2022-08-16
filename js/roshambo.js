fa7214044d4e285f19fa1f1d57197fdae52bb2c8
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-08-16 10:42:36 +0200

    New function to check for 5 wins by either player or computer

--- js/roshambo.js
+++ js/roshambo.js
@@ -4,6 +4,7 @@ window.onload = function() {
   // This variable stores the amount of wins by the computer.
   let computerWins = 0
 
+  // Grabbing all the HTML elements I need to listen to
   const newGameBtn = document.getElementById('newGameButton');
   newGameBtn.addEventListener('click', console.log('new game'))
 
@@ -16,9 +17,27 @@ window.onload = function() {
   const scissorsButton = document.getElementById('scissorsButton')
   scissorsButton.addEventListener('click', setChoices.bind( this, 2))
 
+  // Function will check if either the player or computer have 5 wins and return a message
+  function checkForFiveWins() {
+    if (userWins === 5) {
+      userWins = 0
+      computerWins = 0
+      return console.log('Congratulations you won!')
+    } else if (computerWins === 5) {
+      userWins = 0
+      computerWins = 0
+      return console.log('Sorry, you lost the game!')
+    } else {
+      return
+    }
+  }
+
+  // function will set the choices for the player and computer and then check who wins the round.
+  // It will then run the check for five wins function that will check if either player has now won the game.
   function setChoices(userChoice) {
     let computerChoice = Math.floor(Math.random() * 3)
     checkWinner(userChoice, computerChoice)
+    checkForFiveWins()
   }
 
   // Function that takes two parameters checks who won the round.
@@ -31,40 +50,41 @@ window.onload = function() {
       if (computer == 2) {
       userWins += 1
       console.log('wins: ', userWins, computerWins)
-      return console.log('You win!')
+      return console.log('Computer chose scissors. You win!')
       } else {
       computerWins += 1
       console.log('wins: ', userWins, computerWins)
-      return console.log('You lose!')
+      return console.log('Computer paper. You lose!')
       }
     }
     if (user === 1) {
       if (computer == 0) {
       userWins += 1
       console.log('wins: ', userWins, computerWins)
-      return console.log('You win!')
+      return console.log('Computer chose rock. You win!')
       } else {
       computerWins += 1
       console.log('wins: ', userWins, computerWins)
-      return console.log('You lose!')
+      return console.log('Computer chose scissors. You lose!')
       }
     }
     if (user === 2) {
       if (computer == 1) {
       userWins += 1
       console.log('wins: ', userWins, computerWins)
-      return console.log('You win!')
+      return console.log('Computer chose paper. You win!')
       } else {
       computerWins += 1
       console.log('wins: ', userWins, computerWins)
-      return console.log('You lose!')
+      return console.log('Computer chose rock. You lose!')
       }
     }
   }
 
-  // This function gets a prompt from the user, at the moment a number between 1-3 then reduces it by 1
-  // Then get a random integer between 0-2 for the computer
-  // Then run the function to check who wins the round
+  // Only show new game button if the game hasn't started or if the computer or player just won.
+  // Show user if they tied, lost or won each round.
+  // Show user if they lost or won a game.
+  // Reset wins after either player or computer reach 5 and display new game button.
   
   
 }
